4c7ab29cfb41688436d51a6ef653e17e19f7eb6d
johalloran01 <65722241+johalloran01@users.noreply.github.com>
2025-06-17 18:14:31 -0400

    Added img sources

--- components/card.js
+++ components/card.js
@@ -1,9 +1,12 @@
 export function createCard({ title, text, imageUrl, link }) {
-    const col = document.createElement('div');
-    col.className = 'col-md-4 mb-4';
+  const col = document.createElement('div');
+  col.className = 'col-md-4 mb-4';
 
-    col.innerHTML = `
-    <div class="card h-100 shadow-sm">
+  col.innerHTML = `
+    <div class="card h-100 shadow-sm text-center">
+      <div class="icon-wrapper">
+        <img src="${imageUrl}" alt="${title} Icon" class="icon-img" />
+      </div>
       <div class="card-body d-flex flex-column">
         <h5 class="card-title">${title}</h5>
         <p class="card-text">${text}</p>
@@ -11,5 +14,5 @@ export function createCard({ title, text, imageUrl, link }) {
     </div>
   `;
 
-    return col;
+  return col;
 }
